224cb8bb1e304576bf7c4a549655fb71d531de69
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2021-03-17 10:37:21 +0100

    add locates controller methods

--- app/controllers/locates_controller.rb
+++ app/controllers/locates_controller.rb
@@ -1,2 +1,38 @@
 class LocatesController < ApplicationController
+  before_action :authorized
+
+  def index
+    @locate = Locate.order('created_at DESC')
+    @locates = Locate.new
+    @first_user = User.first
+    @second_user = User.second
+    @third_user = User.third
+  end
+
+  def new
+    @locates = Locate.new
+  end
+
+  def show
+    @user = User.order('created_at DESC')
+    # @idea = @user.ideas
+  end
+
+  #   # POST /users or /users.json
+  def create
+    current_user = User.find(session[:current_user_id])
+    @locate = current_user.locates.new(text: params[:idea])
+
+    respond_to do |format|
+      if @locate.save
+        format.html { redirect_to locates_path, notice: 'Idea successfully shared' }
+      else
+        format.html { redirect_to locates_path, alert: "can't share idea now" }
+      end
+    end
+  end
+
+  def idea_params
+    params.require(:idea).permit(:text)
+  end
 end
