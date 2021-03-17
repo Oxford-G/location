9210a2c6e31d5c970f874d41fcd6f9f5a65d5d42
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2021-03-17 10:17:58 +0100

    add following  model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_17_084858) do
+ActiveRecord::Schema.define(version: 2021_03_17_091558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "followings", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["followed_id"], name: "index_followings_on_followed_id"
+    t.index ["follower_id"], name: "index_followings_on_follower_id"
+  end
 
   create_table "locates", force: :cascade do |t|
     t.integer "author_id"
