180b406dea9cc56c5d3256cc00d5ded12c2b3c36
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2021-03-17 09:52:30 +0100

    add locate model

---
+++ app/models/locate.rb
@@ -0,0 +1,2 @@
+class Locate < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_16_180429) do
+ActiveRecord::Schema.define(version: 2021_03_17_084858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "locates", force: :cascade do |t|
+    t.integer "author_id"
+    t.string "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_locates_on_author_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
