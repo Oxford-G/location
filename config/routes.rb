5c0504addad1605d0255f241d1e6570693f3e0dc
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2021-03-17 10:32:24 +0100

    add locates controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  root to: 'users#sign_in'
+  get 'log-in', to: 'session#log_in'
+  get 'log-out', to: 'session#log_out'
+  resources :followings
   resources :users
+  resources :locates, only: [:create, :index, :show, :new]
+  get '/recovery', to: 'users#recovery'
+  get '/username', to: 'users#_get_username'
+  delete '/unfollow', to: 'followings#unfollow'
 end
